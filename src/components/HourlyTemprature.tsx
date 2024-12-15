9598b4475bbb98c6f2d7bea33b9ba24986ca8498
Ertug Akman <134059022+ertugakmann@users.noreply.github.com>
2024-12-15 20:54:41 +0000

    Add weather details and hourly temperature components to WeatherDashboard

---
+++ src/components/HourlyTemprature.tsx
@@ -0,0 +1,86 @@
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis } from "recharts";
+import { format } from "date-fns";
+import { ForecastData } from "@/api/types";
+
+interface HourlyTempratureProps {
+  data: ForecastData;
+}
+
+const HourlyTemprature = ({ data }: HourlyTempratureProps) => {
+  const chartData = data.list.slice(0, 8).map((item: any) => ({
+    time: format(new Date(item.dt * 1000), "ha"),
+    temp: Math.round(item.main.temp),
+    feels_like: Math.round(item.main.feels_like),
+  }));
+
+  return (
+    <Card className="flex-1">
+      <CardHeader>
+        <CardTitle>Today's Temprature</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="h-[200px] w-full">
+          <ResponsiveContainer width={"100%"} height={"100%"}>
+            <LineChart width={400} height={400} data={chartData}>
+              <XAxis
+                dataKey="time"
+                stroke="#888888"
+                fontSize={12}
+                tickLine={false}
+                axisLine={false}
+                tickFormatter={(value) => `${value}`}
+              />
+              <Tooltip
+                content={({ active, payload }) => {
+                  if (active && payload && payload.length) {
+                    return (
+                      <div className="rounded-lg border bg-background p-2 shadow-sm">
+                        <div className="grid grid-cols-2 gap-2">
+                          <div className="flex flex-col">
+                            <span className="text-0.70rem uppercase text-muted-foreground">
+                              Temprature
+                            </span>
+                            <span className="font-bold">
+                              {payload[0].value}°
+                            </span>
+                          </div>
+                          <div className="flex flex-col">
+                            <span className="text-0.70rem uppercase text-muted-foreground">
+                              Feels Like
+                            </span>
+                            <span className="font-bold">
+                              {payload[1].value}°
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  }
+                  return null;
+                }}
+              />
+              <Line
+                type="monotone"
+                dataKey="temp"
+                stroke="#8884d8"
+                strokeWidth={2}
+                dot={false}
+              />
+              <Line
+                type="monotone"
+                dataKey="feels_like"
+                stroke="#8884d8"
+                strokeWidth={2}
+                dot={false}
+                strokeDasharray="5 5"
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default HourlyTemprature;
--- src/pages/WeatherDashboard.tsx
+++ src/pages/WeatherDashboard.tsx
@@ -1,6 +1,10 @@
+import CurrentWeather from "@/components/CurrentWeather";
+import HourlyTemprature from "@/components/HourlyTemprature";
 import WeatherSkeleton from "@/components/LoadingSkeleton";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
+import WeatherDetails from "@/components/WeatherDetails";
+import WeatherForecast from "@/components/WeatherForecast";
 import { useGeoLocation } from "@/hooks/useGeoLocation";
 import {
   useForecastQuery,
@@ -82,11 +86,30 @@ const WeatherDashboard = () => {
           variant={"outline"}
           size={"icon"}
         >
-          <RefreshCw className="h-4 w-4" />
+          <RefreshCw
+            className={`h-4 w-4 ${
+              weatherQuery.isFetching ? "animate-spin" : ""
+            }`}
+          />
         </Button>
       </div>
 
       {/* Current and Hourly weather */}
+
+      <div className="grid gap-6">
+        <div className="flex flex-col lg:flex-row gap-4">
+          <CurrentWeather
+            data={weatherQuery.data}
+            locationName={locationName}
+          />
+          <HourlyTemprature data={forecastQuery.data} />
+        </div>
+
+        <div className="grid gap-6 md:grid-cols-2 items-start">
+          <WeatherDetails data={weatherQuery.data} />
+          <WeatherForecast data={forecastQuery.data} />
+        </div>
+      </div>
     </div>
   );
 };
