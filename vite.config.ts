529e4e4122248b34556f5b2fd19ac40795f665f8
Ertug Akman <134059022+ertugakmann@users.noreply.github.com>
2024-12-16 23:12:52 +0000

    Update build scripts and Vite configuration for deployment and path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/KLIMATE/", // 'repository-name' kısmını kendi repo isminizle değiştirin
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      path: "path-browserify",
+      url: "url-polyfill",
     },
   },
 });
