52df28b9a8fe8607f391b780581bcb55dac64c07
Ertug Akman <134059022+ertugakmann@users.noreply.github.com>
2024-12-16 23:03:48 +0000

    Add deployment scripts and update Vite configuration for improved path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,12 @@
-import path from "path-browserify"; // path-browserify'yi doğru şekilde import edin
-import "url-polyfill"; // URL modülü için polyfill
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
-
-// __filename ve __dirname'yi tarayıcı ortamında kullanmak için alternatif yöntem
-const __filename = new URL(import.meta.url).pathname; // __filename gibi işlevsellik
-const __dirname = path.dirname(__filename); // __dirname için path.dirname kullanımı
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // src dizinine alias tanımlaması
-      path: "path-browserify", // Node.js path için polyfill
-      url: "url-polyfill", // URL modülü için polyfill
+      "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    outDir: "dist", // Build çıktısı için outDir
   },
 });
