354b78340ca03e690c22575f87f6d3b1c0405cbe
Ertug Akman <134059022+ertugakmann@users.noreply.github.com>
2024-12-16 22:32:29 +0000

    Refactor Vite configuration to use path resolution and set output directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import path from "path";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "./src", // __dirname olmadan çözüm.
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",
   },
 });
