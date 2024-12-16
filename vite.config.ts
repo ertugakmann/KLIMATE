85ed4b1bda6cdb9bada9659ab5ce47ed5e125668
Ertug Akman <134059022+ertugakmann@users.noreply.github.com>
2024-12-16 23:14:41 +0000

    Update Vite configuration base path to lowercase

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/KLIMATE/", // 'repository-name' kısmını kendi repo isminizle değiştirin
+  base: "/klimate/",
   plugins: [react()],
   resolve: {
     alias: {
